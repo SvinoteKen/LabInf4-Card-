38cf037741cd87df865aef4bcb160e87d65fa26d
свин свин <stastab2013@gmail.com>
2020-05-25 16:46:08 +0300

    Add project files.

---
+++ LabInf4 (2)/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LabInf4__2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "oblozka.png");
+            this.imageList1.Images.SetKeyName(1, "6-buben.png");
+            this.imageList1.Images.SetKeyName(2, "7-buben.png");
+            this.imageList1.Images.SetKeyName(3, "8-buben.png");
+            this.imageList1.Images.SetKeyName(4, "9-buben.png");
+            this.imageList1.Images.SetKeyName(5, "10-buben.png");
+            this.imageList1.Images.SetKeyName(6, "valet-buben.png");
+            this.imageList1.Images.SetKeyName(7, "dama-buben.png");
+            this.imageList1.Images.SetKeyName(8, "korol-buben.png");
+            this.imageList1.Images.SetKeyName(9, "tuz-buben.png");
+            this.imageList1.Images.SetKeyName(10, "6-tref.png");
+            this.imageList1.Images.SetKeyName(11, "7-tref.png");
+            this.imageList1.Images.SetKeyName(12, "8-tref.png");
+            this.imageList1.Images.SetKeyName(13, "9-tref.png");
+            this.imageList1.Images.SetKeyName(14, "10-tref.png");
+            this.imageList1.Images.SetKeyName(15, "valet-tref.png");
+            this.imageList1.Images.SetKeyName(16, "dama-tref.png");
+            this.imageList1.Images.SetKeyName(17, "korol-tref.png");
+            this.imageList1.Images.SetKeyName(18, "tuz-tref.png");
+            this.imageList1.Images.SetKeyName(19, "6-pik.png");
+            this.imageList1.Images.SetKeyName(20, "7-pik.png");
+            this.imageList1.Images.SetKeyName(21, "8-pik.png");
+            this.imageList1.Images.SetKeyName(22, "9-pik.png");
+            this.imageList1.Images.SetKeyName(23, "10-pik.png");
+            this.imageList1.Images.SetKeyName(24, "valet-pik.png");
+            this.imageList1.Images.SetKeyName(25, "dama-pik.png");
+            this.imageList1.Images.SetKeyName(26, "korol-pik.png");
+            this.imageList1.Images.SetKeyName(27, "tuz-pik.png");
+            this.imageList1.Images.SetKeyName(28, "6-chervei.png");
+            this.imageList1.Images.SetKeyName(29, "7-chervei.png");
+            this.imageList1.Images.SetKeyName(30, "8-chervei.png");
+            this.imageList1.Images.SetKeyName(31, "9-chervei.png");
+            this.imageList1.Images.SetKeyName(32, "10-chervei.png");
+            this.imageList1.Images.SetKeyName(33, "valet-chervei.png");
+            this.imageList1.Images.SetKeyName(34, "dama-chervei.png");
+            this.imageList1.Images.SetKeyName(35, "korol-chervei.png");
+            this.imageList1.Images.SetKeyName(36, "tuz-chervei.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 585);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ LabInf4 (2)/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabInf4__2_
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        Random rand = new Random();
+        int card = 1;
+        int xpos = 0;
+        int ypos = 0;
+        bool check = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            image = new Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+            Graphics bufimg = Graphics.FromImage(image);
+            bufimg.FillRectangle(new LinearGradientBrush(new Point(0, 0), new Point(ClientRectangle.Width, ClientRectangle.Height), Color.DarkGreen, Color.LightGreen), ClientRectangle);
+            for (int i = 0; i <= 15; i += 3)
+            {
+                bufimg.DrawImage(imageList1.Images[0], 800 + i, 70 + i, 145, 227);
+            }
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                xpos = e.X;
+                ypos = e.Y;
+
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(image, 0, 0);
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (xpos == 0 && ypos == 0)
+            {
+                return;
+            }
+            if (check == false)
+            {
+                return;
+            }
+            Graphics graphics = CreateGraphics();
+            Graphics bufimg = Graphics.FromImage(image);
+            bufimg.TranslateTransform(xpos, ypos);
+            bufimg.RotateTransform(rand.Next(-360, 360));
+            bufimg.TranslateTransform(-(xpos), -(ypos));
+            bufimg.DrawImage(imageList1.Images[card], xpos - 60, ypos - 90, 145, 227);
+            graphics.DrawImage(image, 0, 0);
+            card++;
+            xpos = 0;
+            ypos = 0;
+            check = false;
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.X >= 800 && e.X <= 945 && e.Y >= 70 && e.Y <= 397)
+            {
+                check = true;
+                if (card == 37)
+                {
+                    MessageBox.Show("В колоде закончились карты нажмите F2");
+                }
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F2)
+            {
+                image = new Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+                Graphics bufimg = Graphics.FromImage(image);
+                bufimg.FillRectangle(new LinearGradientBrush(new Point(0, 0), new Point(ClientRectangle.Width, ClientRectangle.Height), Color.LightGreen, Color.DarkGreen), ClientRectangle);
+                for (int i = 0; i <= 15; i += 3)
+                {
+                    bufimg.DrawImage(imageList1.Images[0], 800 + i, 70 + i, 145, 227);
+                }
+                this.Refresh();
+                card = 1;
+                MessageBox.Show("Карты собраны в колоду");
+            }
+        }
+    }
+}
